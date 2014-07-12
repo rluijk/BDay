8110e9163f948e8a2947cd8780a4aa9c7d7b81c3
Eric Tracz <arkadiusz.tracz@gmail.com>
2014-07-12 15:06:52 +0800

    Updating copyright info

--- BDay.php
+++ BDay.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * BDay Plugin for Jomsocial
+ *
+ * @copyright (C) 2014 Eric Tracz All Rights Reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ * @link http://friq.pl
+ **/
 
 defined('_JEXEC') or die('Restricted access');
 require_once JPATH_ROOT .'/components/com_community/libraries/core.php';
@@ -7,8 +14,12 @@ require_once JPATH_ROOT .'/components/com_community/libraries/core.php';
 class plgCommunityBDay extends CApplications
 {
 var $name = "BDay";
-var $_name = 'BDay';
+var $_name = 'bday';
 }
+
 $cuser = CFactory::getUser();
-$data = $cuser->getInfo('FIELD_GENDER');
+$data = $cuser->getInfo('FIELD_BIRTHDATE');
 echo $data;
+
+
+
