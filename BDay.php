abb7cf2430b39fd5df3a2554dd0ba5552ec20c3d
Eric Tracz <arkadiusz.tracz@gmail.com>
2014-07-12 15:11:22 +0800

    Adding age calculating script.

--- BDay.php
+++ BDay.php
@@ -19,7 +19,17 @@ var $_name = 'bday';
 
 $cuser = CFactory::getUser();
 $data = $cuser->getInfo('FIELD_BIRTHDATE');
-echo $data;
 
+echo "$data";
+echo '\r\n';
 
+//date in mm/dd/yyyy format; or it can be in other formats as well
+$birthDate == $data;
+//explode the date to get month, day and year
+$birthDate = explode("/", $birthDate);
+//get age from date or birthdate
+$age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
+    ? ((date("Y") - $birthDate[2]) - 1)
+    : (date("Y") - $birthDate[2]));
+echo "Age is:" . $age;
 
